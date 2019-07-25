a14aa8868748d0c55b9a01a9c0296acc994aa20a
Niko <niko@mlh.io>
2019-07-25 15:28:52 -0400

    Create app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { HashRouter, Switch, Link, Route } from "react-router-dom";
+import Home from "./Home";
+import "./App.css";
+
+const NavBar = () => (
+  <div className="bg-black white ph4 pv2">
+    <div className="flex items-center justify-between">
+      <Link to="/" className="white no-underline">
+        <h1 className="f4 normal">Matchstick</h1>
+      </Link>
+      <Link to="/pricing" className="white no-underline orange">
+        Upgrade Now - Go Pro
+      </Link>
+    </div>
+  </div>
+);
+
+const Pricing = () => <div>Pricing Page</div>;
+
+const NotFound = () => <div>Page Not Found</div>;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="helvetica">
+      <HashRouter>
+        <NavBar />
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/pricing" exact component={Pricing} />
+          <Route path="*" component={NotFound} />
+        </Switch>
+      </HashRouter>
     </div>
   );
 }
