55b37753ad5f3c0aeb5225e66b33a061e755456a
Justin Isaf <jisaf@sapient.com>
2019-07-26 12:31:00 -0400

    start crop

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,9 @@
 import React, { Component } from "react";
 import * as api from "../core/apiActions";
 
+import ReactCrop from "react-image-crop";
+import "react-image-crop/dist/ReactCrop.css";
+
 const baseUrl = process.env.REACT_APP_SERVER_URL;
 
 const LoadingView = () => (
@@ -15,7 +18,13 @@ class Home extends Component {
   state = {
     inputLink: "http://www.google.com",
     outputLink: "http://matchstick.design/",
-    isLoading: false
+    isLoading: false,
+    results: {},
+    crop: {
+      unit: "%",
+      width: 30,
+      aspect: 16 / 9
+    }
   };
 
   changeInputUrl = e => {
@@ -30,12 +39,73 @@ class Home extends Component {
     const { inputLink, outputLink } = this.state;
     this.setState({ isLoading: true });
     const response = await api.fetchReport(inputLink, outputLink);
-    this.setState({ results: response.data, isLoading: false });
+    this.setState({ results: response.data, isLoading: false});
   };
 
+  onImageLoaded = image => {
+    this.imageRef = image;
+  };
+
+  onCropComplete = crop => {
+    this.makeClientCrop(crop);
+  };
+
+  onCropChange = (crop, percentCrop) => {
+    // You could also use percentCrop:
+    // this.setState({ crop: percentCrop });
+    this.setState({ crop });
+  };
+
+  getCroppedImg = (image, crop, fileName) => {
+    const canvas = document.createElement("canvas");
+    const scaleX = image.naturalWidth / image.width;
+    const scaleY = image.naturalHeight / image.height;
+    canvas.width = crop.width;
+    canvas.height = crop.height;
+    const ctx = canvas.getContext("2d");
+
+    ctx.drawImage(
+      image,
+      crop.x * scaleX,
+      crop.y * scaleY,
+      crop.width * scaleX,
+      crop.height * scaleY,
+      0,
+      0,
+      crop.width,
+      crop.height
+    );
+
+    return new Promise((resolve, reject) => {
+      canvas.toBlob(blob => {
+        if (!blob) {
+          //reject(new Error('Canvas is empty'));
+          console.error("Canvas is empty");
+          return;
+        }
+        blob.name = fileName;
+        window.URL.revokeObjectURL(this.fileUrl);
+        this.fileUrl = window.URL.createObjectURL(blob);
+        resolve(this.fileUrl);
+      }, "image/jpeg");
+    });
+  };
+
+  makeClientCrop = async (crop) => {
+    if (this.imageRef && crop.width && crop.height) {
+      const croppedImageUrl = await this.getCroppedImg(
+        this.imageRef,
+        crop,
+        "newFile.jpeg"
+      );
+      this.setState({ croppedImageUrl });
+    }
+  }
+
   render() {
-    const { inputLink, outputLink, results, isLoading } = this.state;
-    return (
+    const { inputLink, outputLink, results, isLoading, crop, croppedImageUrl } = this.state;
+    console.log("loaded", results.comparison)
+return (
       <div>
         <div className="ph4 pv4">
           <h1 className="f1 orange mb1">Design with Confidence.</h1>
@@ -102,6 +172,22 @@ class Home extends Component {
                   style={{ objectFit: "cover", height: "100%" }}
                 />
               )}
+              </div>
+              <div className="w-100 bg-moon-gray ml2" style={{ height: "600px" }}>
+              FOO
+              {results.comparison && (
+          <ReactCrop
+            src={`${baseUrl}${results.comparison}`}
+            crop={crop}
+            onImageLoaded={this.onImageLoaded}
+            onComplete={this.onCropComplete}
+            onChange={this.onCropChange}
+            crossorigin={"anonymous"}
+          />
+        )}
+        {croppedImageUrl && (
+          <img alt="Crop" style={{ maxWidth: "100%" }} src={croppedImageUrl} />
+        )}
             </div>
           </div>
         </div>
