075cff468a4667d2bfdbd930491f1e142fa23812
Justin Isaf <jisaf@sapient.com>
2019-07-27 01:12:26 -0400

    componentize

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,66 +1,39 @@
 import React, {Component} from 'react';
 import * as api from '../core/apiActions';
 
-import ReactCrop from 'react-image-crop';
-import 'react-image-crop/dist/ReactCrop.css';
-
-import * as Trello from 'trello';
+import Ticketing from './Ticketing';
 
 const baseUrl = process.env.REACT_APP_SERVER_URL;
 
-// const Modal = ({results={}, crop, onImageLoaded, onComplete, onChange}) => {
-          {/* <ReactCrop
-            src={`${baseUrl}${results.comparison}`}
-            crop={crop}
-            onImageLoaded={onImageLoaded}
-            onComplete={onComplete}
-            onChange={onChange}
-            crossorigin={'anonymous'}
-          /> */}
-const Modal = (s) => {
-  return (
-    <div className={"vh-100 dt w-100 bg-white absolute"} style={{ opacity: 0.8, zIndex: 99 }}>
-      <div className={"dtc v-mid tc ph3 ph4-l"}>
-        <div className={"white br-25"} style={{maxWidth: "30%"}}>
-          Foo
-          Bar
-          Baz
-          </div>
-      </div>
-    </div>
-  )
-}
-
-const ViewToggle = (props) => {
+const ViewToggle = props => {
   return (
     <div className={`toggle absolute top-0 right-0`}>
-      <input className={"checkbox"} type="checkbox" id="toggle" checked={props.checked} onChange={props.onChange}/>
-      <label htmlFor="toggle" className="toggle"></label>
+      <input
+        className={'checkbox'}
+        type="checkbox"
+        id="toggle"
+        checked={props.checked}
+        onChange={props.onChange}
+      />
+      <label htmlFor="toggle" className="toggle" />
     </div>
-  )
-}
+  );
+};
 
 const LoadingView = () => (
   <div
     className="absolute absolute--fill bg-white pa4 tc"
-    style={{ opacity: 0.8, zIndex: 99 }}
-  >
+    style={{opacity: 0.8, zIndex: 99}}>
     <span className="f4">Generating a snapshot for you. Loading...</span>
   </div>
 );
 class Home extends Component {
   state = {
-    inputLink: "",
-    outputLink: "",
+    inputLink: '',
+    outputLink: '',
     isLoading: false,
     results: {},
-    crop: {
-      unit: '%',
-      width: 30,
-      aspect: 16 / 9
-    },
-    toggleState: 'input',
-    cropIsOpen: false
+    toggleState: 'input'
   };
 
   changeInputUrl = e => {
@@ -78,127 +51,14 @@ class Home extends Component {
     this.setState({results: response.data, isLoading: false});
   };
 
-  onImageLoaded = image => {
-    this.imageRef = image;
-  };
-
-  onCropComplete = crop => {
-    this.makeClientCrop(crop);
-  };
-
-  onCropChange = (crop, percentCrop) => {
-    // You could also use percentCrop:
-    // this.setState({ crop: percentCrop });
-    this.setState({crop});
-  };
-
-  getCroppedImg = (image, crop, fileName) => {
-    const canvas = document.createElement('canvas');
-    const scaleX = image.naturalWidth / image.width;
-    const scaleY = image.naturalHeight / image.height;
-    canvas.width = crop.width;
-    canvas.height = crop.height;
-    const ctx = canvas.getContext('2d');
-
-    ctx.drawImage(
-      image,
-      crop.x * scaleX,
-      crop.y * scaleY,
-      crop.width * scaleX,
-      crop.height * scaleY,
-      0,
-      0,
-      crop.width,
-      crop.height
-    );
-
-    return new Promise((resolve, reject) => {
-      canvas.toBlob(blob => {
-        blob.name = fileName;
-        resolve(blob);
-      }, 'image/jpeg');
-    });
-  };
-
-  makeClientCrop = async crop => {
-    if (this.imageRef && crop.width && crop.height) {
-      const croppedImageBlob = await this.getCroppedImg(
-        this.imageRef,
-        crop,
-        'newFile.jpeg'
-      );
-      let file = new File([croppedImageBlob], 'file.jpg', {
-        type: 'image/jpeg',
-        lastModified: Date.now()
-      });
-      this.setState({croppedImageBlob, file});
-    }
-  };
-
-  cropAndSend = e => {
-    let card = this.createCard();
-    console.log(card);
-    card.then(cardId => {
-      return this.createAndSendForm(this.state.croppedImageBlob, cardId);
-    });
-  };
-
-  createCard = () => {
-    let trello = new Trello(
-      process.env.REACT_APP_TRELLO_KEY,
-      process.env.REACT_APP_TRELLO_TOKEN
-    );
-    return trello
-      .addCard(
-        'Visual Defect',
-        'Please fix the defect',
-        '5d3b226d634ca22ceb838beb'
-      )
-      .then(card => {
-        return card.id;
-      });
-  };
-  // Helper function to build an XMLHttpRequest object that prints to console.log
-  // when a response is received.
-  createRequest = cardId => {
-    var request = new XMLHttpRequest();
-    request.responseType = 'json';
-    request.onreadystatechange = function() {
-      // When we have a response back from the server we want to share it!
-      // https://developer.mozilla.org/en-US/docs/Web/API/XMLHttpRequest/response
-      if (request.readyState === 4) {
-        console.log(`Successfully uploaded at: ${request.response.date}`);
-      }
-    };
-    request.open(
-      'POST',
-      `https://api.trello.com/1/cards/${cardId}/attachments/`
-    );
-    return request;
-  };
-
-  createAndSendForm = (file, cardId) => {
-    let key = process.env.REACT_APP_TRELLO_KEY;
-    let token = process.env.REACT_APP_TRELLO_TOKEN;
-    var formData = new FormData();
-    formData.append('key', key);
-    formData.append('token', token);
-    formData.append('file', file);
-    formData.append('mimeType', 'image/jpg');
-    formData.append('name', 'Defect');
-    // formData.append("mimeType", "image/png"); // Optionally, set mimeType if needed.
-    var request = this.createRequest(cardId);
-    request.send(formData);
-  };
-
   toggleState = () => {
     let nextState = this.state.toggleState === 'input' ? 'output' : 'input';
-    this.setState({toggleState: nextState})
-  }
+    this.setState({toggleState: nextState});
+  };
 
   toggleCrop = () => {
-    this.setState({cropIsOpen: !this.state.cropIsOpen})
-  }
+    this.setState({cropIsOpen: !this.state.cropIsOpen});
+  };
 
   render() {
     const {
@@ -206,26 +66,17 @@ class Home extends Component {
       outputLink,
       results,
       isLoading,
-      crop,
-      croppedImageBlob,
       toggleState
     } = this.state;
     return (
       <div>
-          {this.state.cropIsOpen && (
-        <div className="modal-wrapper">
-          <div className="modal">
-            <Modal 
+        {this.state.cropIsOpen && (
+          <Ticketing
+            baseUrl={baseUrl}
             results={results}
-              crop={crop}
-              onImageLoaded={this.onImageLoaded}
-              onComplete={this.onCropComplete}
-              onChange={this.onCropChange}
-              crossorigin={'anonymous'}
-            />
-          </div>
-        </div>
-                  )}
+            toggleCrop={this.toggleCrop}
+          />
+        )}
         <div className="ph4 pv4">
           <h1 className="f1 orange mb1">Design with Confidence.</h1>
           <p className="f4 measure lh-copy mt0 mid-gray mb3">
@@ -244,7 +95,7 @@ class Home extends Component {
                 onChange={this.changeInputUrl}
                 className="f4 br2 ba b--moon-gray"
                 placeholder="i.e. www.google.stage"
-                style={{ padding: "8px 16px" }}
+                style={{padding: '8px 16px'}}
               />
             </div>
             <div className="pv3 flex flex-column w-100 ml2">
@@ -264,10 +115,7 @@ class Home extends Component {
                 style={{padding: '9px 40px'}}>
                 Submit
               </button>
-              <button
-                onClick={this.toggleCrop}>
-              Crop
-              </button>
+              <button onClick={this.toggleCrop}>Crop</button>
             </div>
           </div>
         </div>
@@ -275,45 +123,37 @@ class Home extends Component {
         <div className="bg-near-white ph4 pv4 b--light-gray">
           <div className="flex bg-white pa3 relative">
             {isLoading && <LoadingView />}
-            <div className="w-100 bg-moon-gray mr2 relative" style={{ height: "600px" }}>
-              {results &&
-                <ViewToggle onChange={this.toggleState} checked={toggleState === 'input'} />
-              }
+            <div
+              className="w-100 bg-moon-gray mr2 relative"
+              style={{height: '600px'}}>
+              {results && (
+                <ViewToggle
+                  onChange={this.toggleState}
+                  checked={toggleState === 'input'}
+                />
+              )}
               <img
                 alt=""
                 className="ba b--light-gray"
                 src={
                   results
                     ? `${baseUrl}${results[toggleState]}`
-                    : require("../images/google.png")
+                    : require('../images/google.png')
                 }
-                style={{ objectFit: "cover", height: "100%" }}
-              />
-            </div>
-            <div className="w-100 bg-moon-gray ml2" style={{ height: "600px" }}>
-              <img
-                alt=""
-                className="ba b--light-gray"
-                style={{ objectFit: "cover", height: "100%" }}
-                src={
-                  results
-                    ? `${baseUrl}${results.comparison}`
-                    : require("../images/comparison.png")
-                }
+                style={{objectFit: 'cover', height: '100%'}}
               />
             </div>
             <div className="w-100 bg-moon-gray ml2" style={{height: '600px'}}>
-
-              {croppedImageBlob && (
-                <div>
-                  <img
-                    alt="Crop"
-                    style={{maxWidth: '100%'}}
-                    src={croppedImageBlob}
-                  />
-                  <button onClick={this.cropAndSend}>Send!</button>
-                </div>
-              )}
+              <img
+                alt=""
+                className="ba b--light-gray"
+                style={{objectFit: 'cover', height: '100%'}}
+                src={
+                  results
+                    ? `${baseUrl}${results.comparison}`
+                    : require('../images/comparison.png')
+                }
+              />
             </div>
           </div>
         </div>
---
+++ src/pages/Ticketing.js
@@ -0,0 +1,199 @@
+import React, {Component} from 'react';
+
+import ReactCrop from 'react-image-crop';
+import 'react-image-crop/dist/ReactCrop.css';
+
+import Trello from 'trello';
+
+class Ticketing extends Component {
+  state = {
+    crop: {
+      unit: '%',
+      width: 30,
+      aspect: 16 / 9
+    },
+    cropIsOpen: false,
+    defectTitle: '',
+    defectDescription: ''
+  };
+
+  onImageLoaded = image => {
+    this.imageRef = image;
+  };
+
+  onCropComplete = crop => {
+    this.makeClientCrop(crop);
+  };
+
+  onCropChange = (crop, percentCrop) => {
+    // You could also use percentCrop:
+    // this.setState({ crop: percentCrop });
+    this.setState({crop});
+  };
+
+  changeDefectTitle = e => {
+    this.setState({defectTitle: e.target.value});
+  };
+
+  changeDefectDescription = e => {
+    this.setState({defectDescription: e.target.value});
+  };
+
+  getCroppedImg = (image, crop, fileName) => {
+    const canvas = document.createElement('canvas');
+    const scaleX = image.naturalWidth / image.width;
+    const scaleY = image.naturalHeight / image.height;
+    canvas.width = crop.width;
+    canvas.height = crop.height;
+    const ctx = canvas.getContext('2d');
+
+    ctx.drawImage(
+      image,
+      crop.x * scaleX,
+      crop.y * scaleY,
+      crop.width * scaleX,
+      crop.height * scaleY,
+      0,
+      0,
+      crop.width,
+      crop.height
+    );
+
+    return new Promise((resolve, reject) => {
+      canvas.toBlob(blob => {
+        blob.name = fileName;
+        resolve(blob);
+      }, 'image/jpeg');
+    });
+  };
+
+  makeClientCrop = async crop => {
+    if (this.imageRef && crop.width && crop.height) {
+      const croppedImageBlob = await this.getCroppedImg(
+        this.imageRef,
+        crop,
+        'newFile.jpeg'
+      );
+      let file = new File([croppedImageBlob], 'file.jpg', {
+        type: 'image/jpeg',
+        lastModified: Date.now()
+      });
+      this.setState({croppedImageBlob, file});
+    }
+  };
+
+  resetDefect = () => {
+    this.setState({defectTitle: '', defectDescription: ''});
+  };
+
+  cropAndSend = e => {
+    let card = this.createCard();
+    card.then(cardId => {
+      this.createAndSendForm(this.state.croppedImageBlob, cardId);
+      this.resetDefect();
+    });
+  };
+
+  createCard = () => {
+    let trello = new Trello(
+      process.env.REACT_APP_TRELLO_KEY,
+      process.env.REACT_APP_TRELLO_TOKEN
+    );
+    return trello
+      .addCard(
+        this.state.defectTitle,
+        this.state.defectDescription,
+        '5d3b226d634ca22ceb838beb'
+      )
+      .then(card => {
+        return card.id;
+      });
+  };
+  // Helper function to build an XMLHttpRequest object that prints to console.log
+  // when a response is received.
+  createRequest = cardId => {
+    var request = new XMLHttpRequest();
+    request.responseType = 'json';
+    request.onreadystatechange = function() {
+      // When we have a response back from the server we want to share it!
+      // https://developer.mozilla.org/en-US/docs/Web/API/XMLHttpRequest/response
+      if (request.readyState === 4) {
+        console.log(`Successfully uploaded at: ${request.response.date}`);
+      }
+    };
+    request.open(
+      'POST',
+      `https://api.trello.com/1/cards/${cardId}/attachments/`
+    );
+    return request;
+  };
+
+  createAndSendForm = (file, cardId) => {
+    let key = process.env.REACT_APP_TRELLO_KEY;
+    let token = process.env.REACT_APP_TRELLO_TOKEN;
+    var formData = new FormData();
+    formData.append('key', key);
+    formData.append('token', token);
+    formData.append('file', file);
+    formData.append('mimeType', 'image/jpg');
+    formData.append('name', 'Defect');
+    // formData.append("mimeType", "image/png"); // Optionally, set mimeType if needed.
+    var request = this.createRequest(cardId);
+    request.send(formData);
+  };
+
+  render() {
+    const {baseUrl, results, toggleCrop} = this.props;
+    const {crop} = this.state;
+    return (
+      <div className="modal-wrapper">
+        <div className="modal flex">
+          <div className={'flex flex-column w-70'}>
+            <ReactCrop
+              src={`${baseUrl}${results.comparison}`}
+              crop={crop}
+              onImageLoaded={this.onImageLoaded}
+              onComplete={this.onCropComplete}
+              onChange={this.onCropChange}
+              crossorigin={'anonymous'}
+            />
+          </div>
+          <div className={'flex flex-column w-30'}>
+            <div className={'pl2 w-80'}>
+              <input
+                value={this.state.defectTitle}
+                onChange={this.changeDefectTitle}
+                className="mb2 pv2 f4 br2 ba b--moon-gray"
+                placeholder="Title"
+                style={{padding: '8px 16px'}}
+              />
+              <textarea
+                value={this.state.defectDescription}
+                onChange={this.changeDefectDescription}
+                className="mb2 pv2 f4 br2 ba b--moon-gray"
+                placeholder="Description"
+                style={{padding: '8px 16px', height: '200'}}
+              />
+              <div>
+                <button
+                  className={'f4 br2 bg-orange bn white'}
+                  style={{padding: '8px 16px'}}
+                  onClick={toggleCrop}>
+                  Cancel
+                </button>
+                <button
+                  className={'f4 br2 bg-orange bn white ml4'}
+                  style={{padding: '8px 16px'}}
+                  onClick={this.cropAndSend}>
+                  Send to Trello
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Ticketing;
