6c2f682aadd4ebaf237fd76c406318f4d2b3adab
Niko <niko@mlh.io>
2019-07-26 22:16:18 -0500

    Update inputs

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -57,8 +57,8 @@ class Home extends Component {
           </p>
         </div>
 
-        <div className="bg-white br3 ph4 pv4 mh4 mb4">
-          <div className="flex ph3 mb3">
+        <div className="bg-white br3 ph4 pv3 mh4 mb3">
+          <div className="flex ph3 pv2">
             <div className="flex flex-column w-100">
               <label className="b mb2">
                 Enter the Invision link to your designs
@@ -71,26 +71,31 @@ class Home extends Component {
                 style={{ padding: "12px 16px" }}
               />
             </div>
-            <div className="flex flex-column w-100 ml2">
-              <label className="b mb2">Enter the link to your website</label>
-              <input
-                value={outputLink}
-                onChange={this.changeOutputUrl}
-                className="f5 br2 ba b--light-gray"
-                placeholder="i.e. www.google.com"
-                style={{ padding: "12px 16px" }}
-              />
-            </div>
-            <div className="ml2 flex flex-column justify-end">
-              <button
-                onClick={this.handleSubmit}
-                className="f5 br2 b bg-orange bn white"
-                style={{ padding: "13px 60px" }}
-              >
-                Submit
-              </button>
+            <div className="flex w-100">
+              <div className="flex flex-column w-100 ml2">
+                <label className="b mb2">Enter the link to your website</label>
+                <input
+                  value={outputLink}
+                  onChange={this.changeOutputUrl}
+                  className="f5 br2 ba b--light-gray"
+                  placeholder="i.e. www.google.com"
+                  style={{ padding: "12px 16px" }}
+                />
+              </div>
+              <div className="ml2 flex flex-column justify-end">
+                <button
+                  onClick={this.handleSubmit}
+                  className="f5 br2 b bg-orange bn white"
+                  style={{ padding: "13px 60px" }}
+                >
+                  Submit
+                </button>
+              </div>
             </div>
           </div>
+        </div>
+
+        <div className="bg-white br3 ph4 pv4 mh4 mb4">
           <div className="flex bg-white pa3 relative">
             {isLoading && <LoadingView />}
             <div className="w-100 bg-moon-gray mr2" style={{ height: "600px" }}>
