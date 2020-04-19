aa6aea01b8c07de4bb3b265f1080bcf7108afe1b
Niko <niko@mlh.io>
2020-04-19 00:17:42 -0400

    Add toggle button

--- sections/home/ResultsSection.js
+++ sections/home/ResultsSection.js
@@ -1,22 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 
 import input from "../../images/matchstick-input.jpg";
 import output from "../../images/matchstick-output.jpg";
 import comparison from "../../images/matchstick-comparison.jpg";
 
-const ResultsSection = () => (
-  <div className="mt4">
-    <div className="flex pt1 mb2">
-      <div className="w-100 mr2">
-        <div className="f6 fw5 mb1">This is how your designs look.</div>
-        <img src={input} className="mb0 br2 mt1" />
-      </div>
-      <div className="w-100 ml1">
-        <div className="f6 fw5 mb1">This is how well it matches.</div>
-        <img src={comparison} className="mb0 br2 mt1" />
+import Toggle from "../../components/Toggle";
+
+const ResultsSection = () => {
+  const [toggle, setToggle] = useState(true);
+  return (
+    <div className="mt4">
+      <div className="flex pt1 mb2">
+        <div className="w-100 mr2">
+          <div className="f6 fw5 mb1 flex items-center">
+            <Toggle onClick={() => setToggle(!toggle)} />
+            {toggle ? (
+              <span>This is how your mockup looks.</span>
+            ) : (
+              <span>This is how your live site looks.</span>
+            )}
+          </div>
+          <img src={toggle ? input : output} className="mb0 br2 mt1" />
+        </div>
+        <div className="w-100 ml1">
+          <div className="f6 fw5 mb1">This is how well it matches.</div>
+          <img src={comparison} className="mb0 br2 mt1" />
+        </div>
       </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default ResultsSection;
