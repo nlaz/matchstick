56aa306aabbafc924f337a23ebf8ac176313ffef
Niko <niko@mlh.io>
2020-05-31 22:22:33 -0400

    Setup new layout

--- sections/home/FormSection.js
+++ sections/home/FormSection.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Upload, Maximize, X } from "react-feather";
+import { Upload, Maximize, ChevronRight, X } from "react-feather";
 import FileBase64 from "react-file-base64";
 
 import Input from "../../components/Input";
@@ -8,25 +8,38 @@ import input from "../../images/matchstick-input.jpg";
 import output from "../../images/matchstick-output.jpg";
 import comparison from "../../images/matchstick-comparison.jpg";
 
-const Options = () => (
-  <div className="ml3" style={{ width: "360px" }}>
-    <div className="f4 fw5 mb3 pb2">Options</div>
-    <div className="mb3 pb2">
-      <label className="f6 db mid-gray mb2">Orientation</label>
-      <Input value="Vertical" />
-    </div>
-    <div className="mb3 pb2 flex">
-      <div className="w-100 mr2">
-        <label className="f6 db mid-gray mb2">Width</label>
-        <Input value="1080px" />
+const Options = () => {
+  const [showOptions, setShowOptions] = useState(true);
+
+  return (
+    <div>
+      <div class="flex justify-between items-center mt5">
+        <div className="f4 fw5">Options</div>
+        <div className="silver b--moon-gray ph1 pointer f7 ba br2 ph2 pv1">
+          Reset
+        </div>
       </div>
-      <div className="w-100 ml2">
-        <label className="f6 db mid-gray mb2">Height</label>
-        <Input value="3080px" />
-      </div>
+      {showOptions && (
+        <div>
+          <div className="mb3 pb1 mt3 pt2">
+            <label className="f6 db mid-gray mb2">Orientation</label>
+            <Input value="Vertical" />
+          </div>
+          <div className="mb3 pb1 flex">
+            <div className="w-100 mr2">
+              <label className="f6 db mid-gray mb2">Width</label>
+              <Input value="1080px" />
+            </div>
+            <div className="w-100 ml2">
+              <label className="f6 db mid-gray mb2">Height</label>
+              <Input value="3080px" />
+            </div>
+          </div>
+        </div>
+      )}
     </div>
-  </div>
-);
+  );
+};
 
 class Form extends React.Component {
   state = { capture: {}, upload: {}, showModal: false };
@@ -61,7 +74,7 @@ class Form extends React.Component {
     const { upload, showModal } = this.state;
     console.log("Upload", upload);
     return (
-      <div className="mr3" style={{ flex: 1 }}>
+      <div className="mr4" style={{ width: "420px" }}>
         {showModal && (
           <ShowModal
             onDismiss={() => this.setShowModal(false)}
@@ -70,16 +83,13 @@ class Form extends React.Component {
             comparison={comparison}
           />
         )}
-        <div className="flex items-end mb4 pb2">
-          <div className="w-50 mr2">
-            <div className="fw5 f6 pb2">Step 1: Share your website</div>
-            <Input
-              placeholder="Enter your website link"
-              className="input-primary"
-            />
+        <div className="mb4 pb2">
+          <div className="w-100 mb3 pb1">
+            <div className="fw5 pb2">Website link</div>
+            <Input placeholder="Enter your website link" />
           </div>
-          <div className="w-25 mr2 relative">
-            <div className="fw5 f6 pb1">Step 2: Upload your designs</div>
+          <div className="w-100 relative mb3 pb1">
+            <div className="fw5 pb2">Upload your designs</div>
             <label
               htmlFor="file-upload"
               className="btn-upload flex items-center w-100 flex justify-center"
@@ -89,7 +99,7 @@ class Form extends React.Component {
             </label>
             <input type="file" id="file-upload" onChange={this.onFileUpload} />
             {Object.keys(upload).length > 0 && (
-              <div className="f6 flex items-center absolute mt1 pv1 w-100">
+              <div className="f6 flex items-center mt1 pt1 w-100">
                 <img
                   src={upload.base64}
                   style={{ width: "20px", height: "20px" }}
@@ -105,30 +115,39 @@ class Form extends React.Component {
               </div>
             )}
           </div>
-          <button className="btn btn-primary flex items-center w-25 flex justify-center">
-            <span className="ml2 ml2">Submit</span>
+          <button className="btn btn-primary flex items-center flex justify-center mt3 ml-auto pointer">
+            <span className="w-100 ml2 pl1">Submit</span>
+            <ChevronRight width={24} />
           </button>
-        </div>
-        <div className="bg-white shadow-4 center pa4 br3 relative">
-          <img src={comparison} />
-          <div
-            className="pointer bg-white absolute flex items-center justify-center shadow-5 top-2 right-2 ma4 br-100"
-            style={{ width: "56px", height: "56px" }}
-            onClick={() => setShowModal(true)}
-          >
-            <Maximize />
-          </div>
+          <Options />
         </div>
       </div>
     );
   }
 }
 
-const FormSection = () => (
-  <div className="flex mb5">
-    <Form />
-    <Options />
+const Results = ({ setShowModal }) => (
+  <div className="bg-white shadow-4 center pa4 br3 relative">
+    <img src={comparison} />
+    <div
+      className="pointer bg-white absolute flex items-center justify-center shadow-5 top-2 right-2 ma4 br-100"
+      style={{ width: "56px", height: "56px" }}
+      onClick={() => setShowModal(true)}
+    >
+      <Maximize />
+    </div>
   </div>
 );
+
+const FormSection = () => {
+  const [showModal, setShowModal] = useState(false);
+
+  return (
+    <div className="flex mb5 ph3">
+      <Form />
+      <Results setShowModal={setShowModal} />
+    </div>
+  );
+};
 
 export default FormSection;
