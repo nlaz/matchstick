75c775f1fab8a0064de442d3764722bf02f9c1bb
Niko <niko@mlh.io>
2020-07-07 15:04:54 -0400

    Add google analytics snippet and update meta data

--- components/Layout.js
+++ components/Layout.js
@@ -1,22 +1,46 @@
-import { Component } from "react";
+import React, { Component } from "react";
 import Head from "next/head";
+import { initGA, logPageView } from "../helpers/analytics";
 
-const Layout = ({ children }) => (
-  <div
-    className="sans-serif bg-washed-blue flex flex-column"
-    style={{ minHeight: "100vh" }}
-  >
-    <Head>
-      <title>Matchstick - A tool to visually check designs</title>
-      <meta
-        name="viewport"
-        content="initial-scale=1.0, width=device-width"
-        key="viewport"
-      />
-      <link rel="shortcut icon" type="image/x-icon" href="/favicon.ico" />
-    </Head>
-    {children}
-  </div>
-);
+class Layout extends Component {
+  componentDidMount() {
+    if (!window.GA_INITIALIZED) {
+      initGA();
+      window.GA_INITIALIZED = true;
+    }
+    logPageView();
+  }
+
+  render() {
+    return (
+      <div
+        className="sans-serif bg-washed-blue flex flex-column"
+        style={{ minHeight: "100vh" }}
+      >
+        <Head>
+          <title>Matchstick - A tool to visually check designs</title>
+          <meta
+            name="viewport"
+            content="initial-scale=1.0, width=device-width"
+            key="viewport"
+          />
+          <meta property="og:title" content="Matchstick" />
+          <meta
+            property="og:description"
+            content="A tool to visually compare your website and mockups to pinpoint the missing details."
+          />
+          <meta
+            name="twitter:description"
+            content="A tool to visually compare your website and mockups to pinpoint the missing details."
+          />
+          <meta property="og:url" content="https://matchstick.design" />
+          <meta property="og:type" content="website" />
+          <link rel="shortcut icon" type="image/x-icon" href="/favicon.ico" />
+        </Head>
+        {this.props.children}
+      </div>
+    );
+  }
+}
 
 export default Layout;
---
+++ helpers/analytics.js
@@ -0,0 +1,16 @@
+import ReactGA from "react-ga";
+
+export const initGA = () => {
+  ReactGA.initialize("UA-144670762-1");
+};
+
+export const logPageView = () => {
+  ReactGA.set({ page: window.location.pathname });
+  ReactGA.pageview(window.location.pathname);
+};
+
+export const logEvent = (category = "", action = "") => {
+  if (category && action) {
+    ReactGA.event({ category, action });
+  }
+};
