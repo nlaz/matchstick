88ba31700cbb1ea152d2967ab3147abbed8992a5
Niko <niko@mlh.io>
2020-07-14 08:08:17 -0400

    Update environment variables

--- helpers/apiActions.js
+++ helpers/apiActions.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseUrl = process.env.REACT_APP_SERVER_URL || "http://localhost:5000";
+const baseUrl = process.env.NEXT_PUBLIC_SERVER_URL || "http://localhost:5000";
 
 export const fetchComparison = async (formData) => {
   return await axios.post(baseUrl + "/api/comparison", formData);
